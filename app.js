aa5a40cfdb250ff4876e6615ff687b7e1896b798
ibadurrehman718 <ibadurrehman718@gmail.com>
2023-10-18 06:54:28 +0500

    bg done

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ form.addEventListener("submit", (e) => {
       `https://api.weatherapi.com/v1/current.json?key=f7402a6bba5949fb957104953231610&q=${input.value}`
     )
     .then((result) => {
-      div.innerHTML = `<div class="data-div"><span class="name-region"><h1 class=" city-name">${result.data.location.name}</h1><div class="con-reg"> <h6 >${result.data.location.region},</h6><h6>${result.data.location.country}</h6></div></span><h1 class="item center">${result.data.current.temp_c} °C</h1 class="item"><h1>${result.data.current.condition.text}</h1><h1 class="item">Humidity: ${result.data.current.humidity}</h1><h1 class="item">${result.data.location.country}</h1><h1 class="item">Wind: ${result.data.current.wind_kph} km/h</h1><h1 class="item">${result.data.location.localtime}</h1></div><div>
-      <img src="${result.data.current.condition.icon}"</div>`;
+      div.innerHTML = `<div class="parent-div"><div class="data-div"><span class="name-region"><h1 class=" city-name">${result.data.location.name}</h1><div class="con-reg"> <h6 >${result.data.location.region},</h6><h6>${result.data.location.country}</h6></div></span><h1 class="item center">${result.data.current.temp_c} °C</h1 class="item"><h1>${result.data.current.condition.text}</h1><h1 class="item">Humidity: ${result.data.current.humidity}</h1><h1 class="item">${result.data.location.country}</h1><h1 class="item">Wind: ${result.data.current.wind_kph} km/h</h1><h1 class="item">${result.data.location.localtime}</h1></div><div>
+      <img src="${result.data.current.condition.icon}"</div></div>`;
     })
     .catch(() => {
       console.log("City is not in our database");
